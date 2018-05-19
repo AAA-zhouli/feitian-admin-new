b46c0ec5e1559bcc8f809b0f5f69dd173e193cee
leoZhou1 <zhouliye66@163.com>
2018-05-19 22:10:41 +0800

    初始化代码

--- src/main/java/com/feitian/admin/Application.java
+++ src/main/java/com/feitian/admin/Application.java
@@ -1,10 +1,5 @@
 package com.feitian.admin;
 
-import com.taobao.api.DefaultTaobaoClient;
-import com.taobao.api.TaobaoClient;
-import com.taobao.api.internal.util.StringUtils;
-import com.taobao.api.request.TradesSoldGetRequest;
-import com.taobao.api.response.TradesSoldGetResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -24,34 +19,11 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(Application.class);
     }
 
-    public static final String appkey = "12253109";
-    public static final String secret = "4313e0a9a0999200c0dfcea224415638";
-    public static final String url = "http://gw.api.taobao.com/router/rest";
-
-
     /**
      */
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
         logger.debug("启动成功");
-        TaobaoClient client = new DefaultTaobaoClient( url, appkey, secret);
-
-        TradesSoldGetRequest req = new TradesSoldGetRequest();
-        req.setFields("tid,type,status,payment,orders,rx_audit_status");
-        req.setStartCreated(StringUtils.parseDateTime("2018-03-01 00:00:00"));
-        req.setEndCreated(StringUtils.parseDateTime("2018-05-01 23:59:59"));
-        req.setStatus("TRADE_FINISHED");
-//        req.setBuyerNick("zhangsan");
-//        req.setType("game_equipment");
-//        req.setExtType("service");
-//        req.setRateStatus("RATE_UNBUYER");
-//        req.setTag("time_card");
-        req.setPageNo(1L);
-        req.setPageSize(40L);
-        req.setUseHasNext(true);
-        TradesSoldGetResponse rsp = client.execute(req, "6101c214f66dc59df23d754ba3cfe13f81fa9a79dead19c1974643988\n");
-        System.out.println(rsp.getBody());
-        logger.debug("返回结果"+rsp.getBody());
     }
 
 
