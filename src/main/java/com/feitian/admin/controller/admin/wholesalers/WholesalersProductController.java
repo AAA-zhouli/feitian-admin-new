38160387f859bcf120a1d81e0494d62826c46ab4
leoZhou1 <zhouliye66@163.com>
2018-05-20 10:41:20 +0800

    初始化代码

--- src/main/java/com/feitian/admin/controller/admin/wholesalers/WholesalersProductController.java
+++ src/main/java/com/feitian/admin/controller/admin/wholesalers/WholesalersProductController.java
@@ -1,24 +1,26 @@
 package com.feitian.admin.controller.admin.wholesalers;
 
+
 import com.feitian.admin.controller.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 档口资金流水
+ * 档口商品管理
  */
 @Controller
-@RequestMapping("/admin/capitalflow")
-public class WholesalersCapitalflowController extends BaseController {
+@RequestMapping("/admin/product")
+public class WholesalersProductController extends BaseController {
 
     /**
-     * 资金流水管理
+     *
      * @return
      */
-    @RequestMapping("/capitalflow/index")
-    public String capitalFlowIndex() {
-        return "admin/capitalflow/index";
+    @RequestMapping("/index")
+    public String rechargeIndex() {
+        return "admin/product/index";
     }
+
 
 
 }
